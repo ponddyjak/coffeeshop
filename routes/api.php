bc1a5cb042e9a0cc8dee33d1aff19b066534be45
Mr. Anderson <chonnawee@softberry.co.th>
2021-05-13 19:14:43 +0700

    rename products => product

--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::post("products",[ProductController::class,'products']);
-Route::post("products/{id}",[ProductController::class,'products']);
+Route::post("products/{id}",[ProductController::class,'product']);
